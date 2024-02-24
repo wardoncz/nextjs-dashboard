d5790090723fbc4e4d8aa4eeb5d8add90522721a
wardoncz <wardoncz87@gmail.com>
2024-02-24 16:23:44 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,30 @@
 import Baloo from 'next/font/local'
 import "./globals.css";
-import Head from 'next/head';
 
 const baloo = Baloo({ src: './Baloo-Regular-webfont.woff' })
 
 export const metadata = {
   title: "Arcade Allies",
   description: "Find and play together",
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/images/favicon-bk.ico',
+        href: '/images/favicon-bk.ico',
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/images/favicon.ico',
+        href: '/images/favicon.ico',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head> {}
-        <link rel="icon" href="/images/favicon.ico" /> {}
-        {}
-        <script>
-          {`
-            const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-            const faviconLink = document.querySelector('link[rel="icon"]');
-            if (faviconLink) {
-              faviconLink.href = prefersDarkMode ? '/images/favicon.ico' : '/images/favicon-bk.ico';
-            }
-          `}
-        </script>
-      </Head>
       <body className={baloo.className}>{children}</body>
     </html>
   );
