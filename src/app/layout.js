bca1f52df15ff67eaa341433406be29cb24d7a3f
wardoncz <wardoncz87@gmail.com>
2024-02-24 15:30:26 +0100

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,20 @@ const baloo = Baloo({ src: './Baloo-Regular-webfont.woff' })
 export const metadata = {
   title: "Arcade Allies",
   description: "Find and play together",
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/images/favicon-or.ico',
+        href: '/images/favicon-or.ico',
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/images/favicon.ico',
+        href: '/images/favicon.ico',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
