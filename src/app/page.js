f201682b631fd952fdc9771f020e6565372de34f
wardoncz <wardoncz87@gmail.com>
2024-02-24 15:00:54 +0100

    Responsive

--- src/app/page.js
+++ src/app/page.js
@@ -18,35 +18,35 @@ export default function Home() {
           <a href="#">CHAT</a>
           <a href="#">FAQ</a>
         </nav>
-        <header className={styles.aboutHeader}>
-          <h1>ARCADE ALLIES</h1>
-          <div></div>
-          <p>is a web platform that connects you with gamers who love the same PC games as you do. You can find, chat, and play with your game mates from all over the world. Join Arcade allies and have more fun gaming.</p>
-          <a href="#">GET STARTED</a>
-          <a href="#" style={{left:"170px"}}>LEARN MORE</a>
-        </header>
-        <div className={styles.stats}>
-          <h1>STATS</h1>
-          <div className={styles.line}></div>
-          <div className={styles.statsCont}>
-            <a href="#">
-              <h1>100</h1>
-              <h2>TOTAL PLAYERS</h2>
-            </a>
-            <a href="#"style={{margin:"0 0 0 219px"}}>
-              <h1>24</h1>
-              <h2>ONLINE PLAYERS</h2>
-            </a>
-            <a href="#" style={{margin:"114px 0 0 0"}}>
-              <h1>20+</h1>
-              <h2>GAMES</h2>
-            </a>
-            <a href="#" style={{margin:"114px 0 0 219px"}}>
-              <h1>4.9</h1>
-              <h2>PLAYER RATINGS</h2>
-            </a>
+          <header className={styles.aboutHeader}>
+            <h1>ARCADE ALLIES</h1>
+            <div></div>
+            <p>is a web platform that connects you with gamers who love the same PC games as you do. You can find, chat, and play with your game mates from all over the world. Join Arcade allies and have more fun gaming.</p>
+            <a href="#">GET STARTED</a>
+            <a href="#" style={{left:"150px"}}>LEARN MORE</a>
+          </header>
+          <div className={styles.stats}>
+            <h1>STATS</h1>
+            <div className={styles.line}></div>
+            <div className={styles.statsCont}>
+              <a>
+                <h1>100</h1>
+                <h2>TOTAL PLAYERS</h2>
+              </a>
+              <a className={styles.statsOnline}>
+                <h1>24</h1>
+                <h2>ONLINE PLAYERS</h2>
+              </a>
+              <a className={styles.statsGames}>
+                <h1>20+</h1>
+                <h2>GAMES</h2>
+              </a>
+              <a className={styles.statsRatings}>
+                <h1>4.9</h1>
+                <h2>PLAYER RATINGS</h2>
+              </a>
+            </div>
           </div>
-        </div>
       </div>
     </main>
   );
